df67c5f3508595883bb4882af74b2758f7c7b587
NotSoSaneExile <sciemastyle@wp.pl>
2021-04-12 11:44:23 +0200

    12.04.2021 funkcje

--- main.c
+++ main.c
@@ -13,11 +13,6 @@
 #include <utime.h> //Pozwala programistom zmieniać i ustawiać datę dostępu oraz datę modyfikacji plików.
 #include <sys/mman.h> //Memory management - będzie potrzebne do mapowania plików void  *mmap(void *, size_t, int, int, int, off_t);
 
-/*Tutaj przydałoby się wypisać prototypy*/
-
-
-
-
 /*Tutaj funkcje
 Potrzebne nam funkcje:
 Pobieranie czasu/rozmiaru/poziomu uprawnień
@@ -25,11 +20,46 @@ Zmiana katalogów
 Usuwanie katalogów
 Sprawdzanie folderów. Pozycje które nie są zwykłymi plikami są ignorowane (np. katalogi i dowiązania symboliczne).*/
 
+
+//Źródło https://man7.org/linux/man-pages/man2/lstat.2.html
+off_t FileSize(const char* in)
+{
+    struct stat fsize;
+    if (stat(in, &fsize) == 0)
+    {
+        return fsize.st_size;
+    }
+    else
+        return -1;
+}
+
+mode_t FileTypeMode (const char* in)
+{
+    struct stat filetypemode;
+    if (stat(in, &filetypemode) != -1)
+    {
+        return filetypemode.st_mode;
+    }
+    else
+        return -1;
+}
+
+time_t FileModificationData(const char* in)
+{
+    struct stat time;
+    if (stat(in, &time) == -1)
+    {
+        exit(EXIT_FAILURE);
+    }
+    return time.st_mtime;
+}
+
+
+
 int main(int argc, char* argv[])
 {
-    if (argc < 5)
+    if (argc < 2)
     {
-        printf("Zbyt mala liczba argumentow wejsciowych!");
         syslog(LOG_ERR, "Zbyt mala liczba argumentow wejsciowych!");
         exit(EXIT_FAILURE);
     }
@@ -48,7 +78,7 @@ int main(int argc, char* argv[])
 
     sid = setsid();
     /*Nowa sesja i grupa procesów*/
-    if (setsid() == -1)
+    if (sid() == -1)
         return -1;
 
     /*Ustaw katalog roboczy na główny*/
@@ -57,7 +87,7 @@ int main(int argc, char* argv[])
 
     /*Zamknij wszystkie pliki otwarte*/
 
-    for (i = 0; i < NR_OPEN; i++)
+    for (i = 0; i < NR_OPEN; i++)//NR_OPEN to trochę overkill, ale spoko.
         close(i);
 
     open("/dev/null", O_RDWR);
@@ -72,7 +102,6 @@ int main(int argc, char* argv[])
     char *directory_path1=NULL, *directory_path2=NULL;
     struct stat s;
 
-    /*Tutaj menu dla demona z funkcjami*/
     while ((choice = getopt(argc, argv, "i:o:c:rs")) != -1)
     {
         switch (choice)
@@ -85,11 +114,11 @@ int main(int argc, char* argv[])
             in = optarg;
             if (stat(in, &s) == 0)
             {
-                if (s.st_mode & S_IFDIR) //sciezka jest katalogiem
+                if (s.st_mode & S_IFDIR) //Ścieżka jest katalogiem
                 {
                     directory_path1 = optarg;
                 }
-                else //sciezka nie jest katalogiem, wywal blad
+                else //Ścieżka nie jest katalogiem
                 {
                     printf("-i: Podany argument nie jest folderem");
                     syslog(LOG_ERR, "Podany argument nie jest folderem");
@@ -102,11 +131,11 @@ int main(int argc, char* argv[])
             out = optarg;
             if (stat(out, &s) == 0)
             {
-                if (s.st_mode & S_IFDIR) //sciezka jest katalogiem
+                if (s.st_mode & S_IFDIR) //Ścieżka jest katalogiem
                 {
                     directory_path2 = optarg;
                 }
-                else //sciezka nie jest katalogiem, wywal blad
+                else //Ścieżka nie jest katalogiem
                 {
                     printf("-o: Podany argument nie jest folderem");
                     syslog(LOG_ERR, "Podany argument nie jest folderem");
@@ -125,7 +154,4 @@ int main(int argc, char* argv[])
         }
     }
 
-
-
-    /*Dalsza część maina z daemonem*/
 }
